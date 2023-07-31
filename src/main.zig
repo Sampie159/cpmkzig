75e0cfa2b9eaeab424cdcd057c3d91d643e4f46d
Sampie159 <38163547+Sampie159@users.noreply.github.com>
2023-07-31 12:48:05 -0300

    feat: moved to zig 0.11, added zig-clap to handle cli args

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,51 @@
 const std = @import("std");
-const cpmk = @import("cpmk.zig");
+const clap = @import("clap");
+const Cpmk = @import("cpmk.zig").Cpmk;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    const params = comptime clap.parseParamsComptime(
+        \\-l, --language <LANG>          "Language to use in the new project: c or cpp"
+        \\-p, --project_name <PROJ_NAME> "Name of the new project"
+        \\-h, --help                     "Prints help information"
+    );
 
-    var args = std.process.args();
-    _ = args.next();
-    var lang = args.next() orelse {
-        try stdout.print("usage: cpmk <lang> <proj_name>\n", .{});
-        return;
-    };
-    var proj_name = args.next() orelse {
-        try stdout.print("usage: cpmk <lang> <proj_name>\n", .{});
-        return;
+    const parsers = comptime .{
+        .LANG = clap.parsers.string,
+        .PROJ_NAME = clap.parsers.string,
     };
 
-    var proj = cpmk.Cpmk.new(&lang, &proj_name);
-    try proj.setup_project();
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, parsers, .{
+        .diagnostic = &diag,
+    }) catch |err| {
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+
+    if (res.args.help != 0) {
+        std.debug.print(
+            \\Usage: cpmk [OPTIONS]
+            \\
+            \\Options:
+            \\  -l, --language <LANG>          Language to use in the new project: c or cpp
+            \\  -p, --project_name <PROJ_NAME> Name of the new project
+            \\  -h, --help                     Prints help information
+            \\
+        , .{});
+        return;
+    }
+
+    var lang = res.args.language orelse {
+        std.debug.print("Missing required argument: --language\n", .{});
+        return;
+    };
+
+    var project_name = res.args.project_name orelse {
+        std.debug.print("Missing required argument: --project_name\n", .{});
+        return;
+    };
+
+    const cpmk = Cpmk.new(&lang, &project_name);
+    try cpmk.setup_project();
 }
